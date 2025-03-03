a1729809ddee936727413cf933dd73e19af6509a
anshul <anshul.wiselap@gmail.com>
2025-03-03 15:44:36 +0530

    externalize api gateway username and password

--- src/main/java/com/swift/microgateway/swift_microgateway/configuration/SecurityConfig.java
+++ src/main/java/com/swift/microgateway/swift_microgateway/configuration/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.swift.microgateway.swift_microgateway.configuration;
 
+import com.swift.microgateway.swift_microgateway.common.AESEncryptionHelper;
+import com.swift.microgateway.swift_microgateway.common.PropertiesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,22 +18,35 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    PropertiesService  propertiesService;
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-                // Disable CSRF because this is a stateless API gateway
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                // Configure endpoint permissions
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Disable CSRF for API Gateway
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/auth", "/refresh-token").permitAll()
-                        .anyExchange().authenticated()
+                                              .anyExchange().authenticated() // All Other Routes Require Authentication
                 )
-                // Configure HTTP Basic authentication using the default settings
-                .httpBasic(withDefaults())
+                .httpBasic(withDefaults()) // Enable Basic Auth
                 .build();
-
-
-
     }
 
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+         final String username = propertiesService.getPropertyValue("external.gateway-user-name");
+         final String password = AESEncryptionHelper.decrypt(propertiesService.getPropertyValue("external.gateway-secret"),propertiesService.getPropertyValue("external.gateway-secret-key"));
+
+         UserDetails user = User.withUsername(username)
+                .password(passwordEncoder.encode(password))  // Encode password
+                .roles("USER")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
